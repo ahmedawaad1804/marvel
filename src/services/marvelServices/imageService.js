2a82c4373bf972d07a78ceb75f712a6649e67b0a
ahmed <ahmedawaad1804@gmail.com>
2021-09-27 11:53:35 +0200

    https fix for ios simulator

--- src/services/marvelServices/imageService.js
+++ src/services/marvelServices/imageService.js
@@ -2,6 +2,8 @@ import marvelImageAxiosInstance from './marvelImageAxiosInstance';
 
 export default {
   getCharacterPhoto: (url, size) => {
+    url = url.slice(0, 4) + 's' + url.slice(4);
+    console.log('url', url);
     return marvelImageAxiosInstance.get(url + size);
   },
 };
